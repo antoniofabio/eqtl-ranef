ba36a2c6264024d3c9d5c19b056f41dbbb5416cf
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2014-04-18 15:39:33 -0400

    add testing code to repo

---
+++ tests/test-000.sh
@@ -0,0 +1,1 @@
+../reduce-eqtl-ranef --outcomes=data/expression.thin --fixef=data/fixef.tab --ranef=data/ranef.tab --genespos=data/genespos.sqlite --snpspos=data/snpspos.sqlite --cis-pvalue=1 --trans-pvalue=1 < data/genotype.bySample.thin
