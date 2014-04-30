ae79d4c2b4f183e98b53c55cbff085333ea879f5
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2014-04-30 13:21:21 -0400

    optionally select regressors reading chunk size

---
+++ tests/test-004.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+set -e
+set -u
+
+TMPD=`mktemp -d`
+trap "rm -rf ${TMPD}" EXIT
+
+../generate-eqtl-ranef-data --output=${TMPD} \
+  --regressors=100 --outcomes=1 --samples=30 --fixef=1
+
+../eqtl-ranef \
+  --regressors=${TMPD}/regressors \
+  --outcomes=${TMPD}/outcomes \
+  --fixef=${TMPD}/fixef \
+  --ranef=${TMPD}/ranef \
+  --genespos=${TMPD}/genespos.sqlite \
+  --snpspos=${TMPD}/snpspos.sqlite \
+  --cis-pvalue=0.1 --trans-pvalue=1e-3 \
+  --regressors-chunk-size=301 \
+    | sort
