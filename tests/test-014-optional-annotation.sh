c761be151360854cda2fca62ec9c6541373d06a3
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2014-08-16 11:14:32 -0400

    simplify test on optional annotation

--- tests/test-014-optional-annotation.sh
+++ tests/test-014-optional-annotation.sh
@@ -10,6 +10,7 @@ set -o pipefail
 ../eqtl-ranef \
     --regressors=data/genotype.bySample.thin \
     --outcomes=data/expression.thin \
-    --intef=data/intef.tab \
+    --fixef=data/fixef.tab \
+    --ranef=data/ranef.tab \
     --trans-pvalue=1 \
     | sort
