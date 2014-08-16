24fcc709108c446a2362bb90fda6cc4ff2fbb724
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2014-08-16 06:34:03 -0400

    give meaningful names to tests

--- tests/test-010.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-## do not specify 'fixef' terms
-
-set -e
-set -u
-set -o pipefail
-
-../eqtl-ranef --regressors=data/genotype.bySample.thin --outcomes=data/expression.thin --ranef=data/ranef.tab --genespos=data/genespos.sqlite --snpspos=data/snpspos.sqlite --cis-pvalue=1 --trans-pvalue=1 | sort
