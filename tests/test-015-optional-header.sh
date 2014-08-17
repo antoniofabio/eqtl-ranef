e4d98940387369c2eda761c48194391c7c7dce7e
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2014-08-17 03:21:18 -0400

    optionally print out table header

---
+++ tests/test-015-optional-header.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+set -e
+set -u
+set -o pipefail
+
+../eqtl-ranef \
+    --regressors=data/genotype.bySample.thin \
+    --outcomes=data/expression.thin \
+    --fixef=data/fixef.tab \
+    --ranef=data/ranef.tab \
+    --genespos=data/genespos.sqlite \
+    --snpspos=data/snpspos.sqlite \
+    --cis-pvalue=1 \
+    --trans-pvalue=1 \
+    --header
