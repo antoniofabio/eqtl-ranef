895647258f5744bbada9d7fd092c4c93b2aa30ed
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2014-11-03 03:49:24 -0500

    smarter prefiltering test

--- tests/test-006-prefilter-full.sh
+++ tests/test-006-prefilter-full.sh
@@ -4,6 +4,9 @@ set -e
 set -u
 set -o pipefail
 
+TMPD=`mktemp -d`
+trap "rm -rf ${TMPD}" EXIT
+
 ../eqtl-ranef \
   --regressors=data/genotype.bySample.thin \
   --outcomes=data/expression.thin \
@@ -13,4 +16,23 @@ set -o pipefail
   --genespos=data/genespos.sqlite \
   --snpspos=data/snpspos.sqlite \
   --cis-pvalue=1.0 --trans-pvalue=1.0 \
-    | sort
+  | sort \
+  > ${TMPD}/prefiltered
+
+../eqtl-ranef \
+  --regressors=data/genotype.bySample.thin \
+  --outcomes=data/expression.thin \
+  --fixef=data/fixef.tab \
+  --ranef=data/ranef.tab \
+  --genespos=data/genespos.sqlite \
+  --snpspos=data/snpspos.sqlite \
+  --cis-pvalue=1.0 --trans-pvalue=1.0 \
+  | sort \
+  > ${TMPD}/naive
+
+if diff ${TMPD}/{prefiltered,naive} ; then
+    echo "outputs are identical"
+else
+    echo "outputs differ!"
+fi
+
