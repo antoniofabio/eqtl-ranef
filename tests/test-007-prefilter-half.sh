2f54d588a79c9e67df7cbc2a48cf8fc9ddd7e461
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2014-11-03 04:51:25 -0500

    smarter prefiltering test

--- tests/test-007-prefilter-half.sh
+++ tests/test-007-prefilter-half.sh
@@ -4,6 +4,9 @@ set -e
 set -u
 set -o pipefail
 
+TMPD=`mktemp -d`
+trap "rm -rf ${TMPD}" EXIT
+
 ../eqtl-ranef \
   --regressors=data/genotype.bySample.thin \
   --outcomes=data/expression.thin \
@@ -13,4 +16,34 @@ set -o pipefail
   --genespos=data/genespos.sqlite \
   --snpspos=data/snpspos.sqlite \
   --cis-pvalue=1.0 --trans-pvalue=1.0 \
-    | sort
+  | sort \
+  > ${TMPD}/with-prefiltering
+
+../eqtl-ranef \
+  --regressors=data/genotype.bySample.thin \
+  --outcomes=data/expression.thin \
+  --fixef=data/fixef.tab \
+  --ranef=data/ranef.tab \
+  --genespos=data/genespos.sqlite \
+  --snpspos=data/snpspos.sqlite \
+  --cis-pvalue=1.0 --trans-pvalue=1.0 \
+  | sort \
+  > ${TMPD}/full
+
+awk -v FS='	' -v OFS='	' '{print $2":::"$1}' data/prefilter.half.tab \
+ | sort > ${TMPD}/prefilter.sorted
+
+awk -v FS='	' -v OFS='	' '{print $1":::"$2, $0}' ${TMPD}/full \
+ | sort -k1,1 \
+ > ${TMPD}/full.sorted
+
+join -t '	' ${TMPD}/prefilter.sorted ${TMPD}/full.sorted \
+  | cut -f 2- \
+  | sort \
+  > ${TMPD}/full.postfiltered
+
+if diff ${TMPD}/{with-prefiltering,full.postfiltered} ; then
+  echo "outputs are identical"
+else
+  diff ${TMPD}/{with-prefiltering,full.postfiltered}
+fi
